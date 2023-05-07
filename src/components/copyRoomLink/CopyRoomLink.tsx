7915dcd3083ef33f38de3ddb05e9947c9bd2be6e
agha-huseynov <agha.huseynov@gamesys.ee>
2023-05-07 17:05:05 +0300

    added copyToClipboard function

--- src/components/copyRoomLink/CopyRoomLink.tsx
+++ src/components/copyRoomLink/CopyRoomLink.tsx
@@ -8,9 +8,13 @@ export const CopyRoomLink = ({ roomStatus }: CopyRoomLinkProps) => {
         return <></>
     }
 
+    const copyToClipboard = () => { 
+        navigator.clipboard.writeText(window.location.href);
+    }
+
     return <>
         <h1 className="text-3xl font-bold text-center">Invite players to the room</h1>
         <p className="py-6 text-center">Send the link below to your team mates and soon they will start joining the room</p>
-        <button className="btn btn-outline">Copy Room Link</button>
+        <button onClick={copyToClipboard} className="btn btn-outline">Copy Room Link</button>
     </>
 }
