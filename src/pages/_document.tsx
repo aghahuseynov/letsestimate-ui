3ba1990de08d6574ae3cf7eb590423cc3898ff59
agha-huseynov <agha.huseynov@gamesys.ee>
2023-06-22 23:22:18 +0300

    fix theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html data-theme={getTheme()} lang="en">
+    <Html lang="en">
       <Head>
         <title>{`Let's Estimate`}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
