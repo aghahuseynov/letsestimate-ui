1caa0e6458faa1a7f29c4981916ae382eca51842
agha-huseynov <agha.huseynov@gamesys.ee>
2023-06-25 18:53:36 +0300

    room page working with customhook

--- src/pages/room/[roomName].tsx
+++ src/pages/room/[roomName].tsx
@@ -9,24 +9,14 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { CopyRoomLink } from "@/components/copyRoomLink/CopyRoomLink";
 import { StartEstimation } from "@/components/startEstimation/StartEstimation";
+import useRoomInfo from "@/hooks/useRoomInfo";
 
-
-export async function getServerSideProps({ params }: any) {
-    const response = await fetch(`${getServerURI()}/room/${params.roomName}`)
-    const roomData = await response.json();
-
-    return { props: { room: roomData } }
-}
-
-type RoomProps = {
-    room: RoomType;
-};
-
-const Room = ({ room }: RoomProps) => {
+const Room = () => {
+    const room = useRoomInfo()
     const router = useRouter();
     const socket = useSocket();
 
-    const [roomData, setRoomData] = useState<RoomType>(room);
+    const [roomData, setRoomData] = useState(room);
     const [attendersEstimation, setAttendersEstimation] = useState<RoomEstimation>();
 
     const roomName = router.query.roomName as string
@@ -34,55 +24,61 @@ const Room = ({ room }: RoomProps) => {
     const { playerName, setPlayerName } = useAppContext();
 
     useEffect(() => {
+        setRoomData(room);
+    }, [room])
 
-        const roomInfoRaw = localStorage.getItem(roomName)
-
-        if (!playerName && roomInfoRaw) {
-            const roomInfo = JSON.parse(roomInfoRaw) as unknown as Attender;
-            setPlayerName(roomInfo.playerName);
-            socket.emit('joinRoom', { roomName: router.query.roomName, playerName: playerName, isAdmin: roomInfo.isAdmin });
-            setRoomData({ ...roomData, attenders: [...roomData.attenders, { playerName: roomInfo.playerName, isAdmin: roomInfo.isAdmin }] })
-
-        }
-        else if (!playerName && !roomInfoRaw) {
-            const newPlayerName = prompt('Please enter your name');
-            setPlayerName(newPlayerName!);
-
-            setRoomData({ ...roomData, attenders: [...roomData.attenders, { playerName: newPlayerName!, isAdmin: false }] })
-
-            localStorage.setItem(roomName, JSON.stringify({ roomName: roomName, playerName: newPlayerName, isAdmin: false }));
-
-            socket.emit('joinRoom', { roomName: router.query.roomName, playerName: newPlayerName });
-
-        } else {
-            const roomInfo = JSON.parse(roomInfoRaw!) as unknown as Attender;
-
-            socket.emit('joinRoom', { roomName: router.query.roomName, playerName: playerName, isAdmin: roomInfo.isAdmin });
-        }
-
-        socket.on('newAttenders', async (roomInfo: RoomType) => {
-            setRoomData(roomInfo);
-        })
-
-
-        socket.on('showSize', (data) => {
-            setAttendersEstimation(data);
-        })
-
-
-        socket.on('changeRoomStatus', (roomInfo: RoomType) => {
-            setRoomData(roomInfo);
-        })
-
-        socket.on('sendSelectedSize', (data) => {
-            setAttendersEstimation(data);
-        })
-
-
-    }, [playerName, router.query.roomName])
 
     useEffect(() => {
-        if (roomData.roomStatus === 'start' && attendersEstimation) {
+        if (room && roomData) {
+            const roomInfoRaw = localStorage.getItem(roomName)
+
+            if (!playerName && roomInfoRaw) {
+                const roomInfo = JSON.parse(roomInfoRaw) as unknown as Attender;
+                setPlayerName(roomInfo.playerName);
+                socket.emit('joinRoom', { roomName: router.query.roomName, playerName: playerName, isAdmin: roomInfo.isAdmin });
+                setRoomData({ ...roomData, attenders: [...roomData.attenders, { playerName: roomInfo.playerName, isAdmin: roomInfo.isAdmin }] })
+
+            }
+            else if (!playerName && !roomInfoRaw) {
+                const newPlayerName = prompt('Please enter your name');
+                setPlayerName(newPlayerName!);
+
+                setRoomData({ ...roomData, attenders: [...roomData.attenders, { playerName: newPlayerName!, isAdmin: false }] })
+
+                localStorage.setItem(roomName, JSON.stringify({ roomName: roomName, playerName: newPlayerName, isAdmin: false }));
+
+                socket.emit('joinRoom', { roomName: router.query.roomName, playerName: newPlayerName });
+
+            } else {
+                const roomInfo = JSON.parse(roomInfoRaw!) as unknown as Attender;
+
+                socket.emit('joinRoom', { roomName: router.query.roomName, playerName: playerName, isAdmin: roomInfo.isAdmin });
+            }
+
+            socket.on('newAttenders', (roomInfo: RoomType) => {
+                setRoomData(roomInfo);
+            })
+
+
+            socket.on('showSize', (data) => {
+                setAttendersEstimation(data);
+            })
+
+
+            socket.on('changeRoomStatus', (roomInfo: RoomType) => {
+                setRoomData(roomInfo);
+            })
+
+            socket.on('sendSelectedSize', (data) => {
+                setAttendersEstimation(data);
+            })
+        }
+
+
+    }, [playerName, router.query.roomName, roomData, room])
+
+    useEffect(() => {
+        if (roomData && roomData.roomStatus === 'start' && attendersEstimation) {
             setAttendersEstimation(undefined);
         }
     }, [roomData])
@@ -97,13 +93,13 @@ const Room = ({ room }: RoomProps) => {
     }
 
     const isAdmin = () => {
-        const attender = roomData.attenders.find(q => q.playerName === playerName);
+        const attender = roomData?.attenders.find(q => q.playerName === playerName);
         return attender?.isAdmin
     }
 
     const changeRoomStatus = () => {
         return new Promise((resolve) => {
-            socket.emit('changeRoomStatus', { roomName: roomData.roomName }, (roomInfo: { room: RoomType, roomEstimations: any }) => {
+            socket.emit('changeRoomStatus', { roomName: roomData?.roomName }, (roomInfo: { room: RoomType, roomEstimations: any }) => {
                 setRoomData(roomInfo.room);
                 setAttendersEstimation(roomInfo.roomEstimations);
                 resolve(true);
@@ -113,6 +109,10 @@ const Room = ({ room }: RoomProps) => {
 
     const emitSelectedEstimationSize = (size: string) => {
         socket.emit('sendSelectedSize', { roomName: roomName, selectedEstimationSize: size, playerName: playerName })
+    }
+
+    if (!roomData) {
+        return <div>Loading...</div>
     }
 
     return <div className="flex items-center">
