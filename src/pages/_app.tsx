72bdf9755e9d75f801f17729da064e618f3b7739
meltemhuseynov <meltem.mazioglu@gmail.com>
2023-09-28 22:25:21 +0300

    navbar changed and Theme changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,55 +1,21 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { Theme } from '@/components/theme/Theme'
-import { AppProvider } from '@/context/appContext'
-import { SocketProvider } from '@/context/socketContext'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProvider } from "@/context/appContext";
+import { SocketProvider } from "@/context/socketContext";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "../styles/_app.css";
+import { Navbar } from "@/components/navbar/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <SocketProvider>
-    <AppProvider>
-      <div className="navbar bg-neutral text-neutral-content">
-
-        <div className="navbar-start">
-          <a className="btn btn-ghost normal-case text-xl">{`Let's Estimate`}</a>
+  return (
+    <SocketProvider>
+      <AppProvider>
+        <div className="App">
+          <Navbar />
+          <Component {...pageProps} />
         </div>
-        <div className='navbar-center'>
-          <a href='https://github.com/aghahuseynov/letsestimate-ui' target='_blank' className='btn' rel="noreferrer" >
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill='white' width="24" height="24" viewBox="0 0 24 24">
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-              </svg>
-            </span>
-            <span>
-              &nbsp; UI
-            </span>
-          </a>
-          <a href='https://github.com/aghahuseynov/letsestimate-service' target='_blank' className='btn ' rel="noreferrer" >
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill='white' width="24" height="24" viewBox="0 0 24 24">
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-              </svg>
-            </span>
-            <span>
-              &nbsp; Service
-            </span>
-          </a>
-        </div>
-        <div className="navbar-end">
-          <Theme />
-          <a href="https://www.buymeacoffee.com/aghahuseynov">
-            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=aghahuseynov&button_colour=BD5FFF&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00" />
-          </a>
-        </div>
-      </div>
-      <Component {...pageProps} />
-      <footer className="fixed bottom-0 footer items-center p-4 bg-neutral text-neutral-content">
-        <div className="items-center grid-flow-col">
-          <p>Copyright © 2023 - All right are NOT reserved, you can use it as open source from github :)</p>
-          <p>Contact - <a href="mailto:agahuseynov@hotmail.com">agahuseynov@hotmail.com</a> </p>
-        </div>
-      </footer>
-    </AppProvider>
-  </SocketProvider>
+      </AppProvider>
+    </SocketProvider>
+  );
 }
