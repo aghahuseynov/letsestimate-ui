a88373f0f6d51c3435f2cb0b9d3383e2049233b4
meltemhuseynov <meltem.mazioglu@gmail.com>
2023-10-01 13:19:20 +0300

    about modal changed and donation file deleted

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Modal from "../modal/Modal";
 import useModal from "../modal/useModal";
-import {BsQuestionCircle} from 'react-icons/bs'
+import { BsQuestionCircle } from "react-icons/bs";
 import AboutStyles from "./about.module.css";
-
 
 const About = () => {
   const { isOpen, toggle } = useModal();
 
   return (
-    < >
-      <button className='btn-blank' onClick={toggle}>
-        <div className={AboutStyles.aboutIcon}>   <BsQuestionCircle/></div>
-      
+    <>
+      <button className="btn-blank" onClick={toggle}>
+        <div className={AboutStyles.aboutIcon}>
+          <BsQuestionCircle />
+        </div>
       </button>
       <Modal title="About" isOpen={isOpen} toggle={toggle}>
-        <p>about</p>
+        <p className={AboutStyles.subtitle}>
+          Let`s Estimate is an open-source and free application that relies on
+          financial support to remain accessible and ensure timely fixes for new
+          features and bugs. The current monthly expense for maintaining the
+          project is only 5 euros.
+        </p>
+        <p className={AboutStyles.subtitle}>
+          By donating just 5 euros, you can help sustain the website and support
+          its long-term development. Please feel free to click the `Buy me a
+          coffee` button to make a donation. Your support is greatly
+          appreciated!
+        </p>
       </Modal>
     </>
   );
