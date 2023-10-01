14615aba3d4553b569ffec1f7ce50503c256d19f
meltemhuseynov <meltem.mazioglu@gmail.com>
2023-10-01 16:10:36 +0300

    fix pipeline build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import { CreateRoom } from "@/components/createRoom/CreateRoom";
-import { DonationInfoModal } from "@/components/donationInfoModal/DontationInfoModal";
 
 export default function Home() {
   return (
     <>
-      <DonationInfoModal />
       <CreateRoom />
     </>
   );
