a671eaa84e8fad0b9bfc8774c9da7434dc2383a4
meltemhuseynov <meltem.mazioglu@gmail.com>
2023-10-01 22:31:08 +0300

    copy alert added

--- src/components/copyRoomLink/CopyRoomLink.tsx
+++ src/components/copyRoomLink/CopyRoomLink.tsx
@@ -1,24 +1,41 @@
-import { RoomStatusType } from "@/common/types"
+import { RoomStatusType } from "@/common/types";
 import CopyRoom from "./CopyRoomLink.module.css";
-
+import { useState } from "react";
 
 type CopyRoomLinkProps = {
-    roomStatus: RoomStatusType
-}
+  roomStatus: RoomStatusType;
+};
 
 export const CopyRoomLink = ({ roomStatus }: CopyRoomLinkProps) => {
+  const [copyAllert, setCopyAllert] = useState<boolean>(false);
 
-    if (roomStatus !== 'start') {
-        return <></>
-    }
+  if (roomStatus !== "start") {
+    return <></>;
+  }
 
-    const copyToClipboard = () => {
-        navigator.clipboard.writeText(window.location.href);
-    }
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setCopyAllert(!copyAllert);
+    setTimeout(() => {
+      setCopyAllert(false);
+    }, 3000);
+  };
 
-    return <>
-        <h1 className={CopyRoom.title}>Invite players to the room</h1>
-        <p  className={CopyRoom.subtitle}>Send the link below to your team mates and soon they will start joining the room</p>
-        <button onClick={copyToClipboard} className="btn">Copy Room Link</button>
+  return (
+    <>
+      <h1 className={CopyRoom.title}>Invite players to the room</h1>
+      <p className={CopyRoom.subtitle}>
+        Send the link below to your team mates and soon they will start joining
+        the room
+      </p>
+      <button onClick={copyToClipboard} className="btn">
+        Copy Room Link
+      </button>
+      {copyAllert && (
+        <div id={copyAllert ? "allertPassword" : "allertPassword active"}>
+          {"Copied!"}
+        </div>
+      )}
     </>
-}
+  );
+};
